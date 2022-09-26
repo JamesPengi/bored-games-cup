88e54111664ffc26fde9d97ea4a100c34812a1d0
Joshua Fernandes <fernandes.josh@gmail.com>
2022-09-26 13:32:18 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BG Cup Draft Website
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>BG Cup Draft | Bored Games</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>BG Cup Draft</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
