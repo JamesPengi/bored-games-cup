f070616c3b68342afe64cbcff2898c6643f6fa85
Joshua Fernandes <fernandes.josh@gmail.com>
2022-09-28 10:37:13 -0400

    feat: added isActiveDraft column

---
+++ prisma/migrations/20220928143639_add_is_active_draft/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Game" ADD COLUMN     "isActiveDraft" BOOL NOT NULL DEFAULT true;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { trpc } from '../utils/trpc';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const { mutate } = trpc.useMutation('game.create', {
+    onSuccess(data) {
+      router.push(`/${data}/draft`);
+    },
+  });
+
   return (
     <>
       <Head>
@@ -9,8 +18,21 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>BG Cup Draft</h1>
+      <main className="flex flex-col min-h-screen justify-center text-white bg-slate-800 p-4">
+        <h1 className="font-bold text-3xl text-center">BG Cup Draft</h1>
+        <span className="text-center">
+          Use /create in the #bg-cup-1 text channel
+        </span>
+        <button
+          onClick={() => {
+            mutate({
+              blueTeamId: 'cl8ke5kw300003n6l0w7npbjx',
+              redTeamId: 'cl8ke5kw300023n6lo9bqia0k',
+            });
+          }}
+        >
+          Create Game
+        </button>
       </main>
     </>
   );
